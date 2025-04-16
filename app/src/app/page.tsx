500d16fd7ad5825ea465e018c65269153d5e6306
Kim Guo <kbrqin@gmail.com>
2025-04-15 23:22:07 -0400

    some final styling

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,7 +6,7 @@ import SignInWithGoogleButton from "@/components/SignInWithGoogleButton";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 w-[200px] items-center sm:items-start">
         <span className="font-semibold">🐋 welcom to speedcal &gt;:&#41;</span>
         <UserGreetText />
         {/* <Button onClick={() => redirect('/calendar')}>open calendar</Button> */}
--- app/src/components/SignInWithGoogleButton.tsx
+++ app/src/components/SignInWithGoogleButton.tsx
@@ -1,21 +1,49 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { signInWithGoogle } from "@/lib/auth-actions";
-import React from "react";
+import { createClient } from "@/utils/supabase/client";
+import React, { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 const SignInWithGoogleButton = () => {
-  return (
-    <Button
-      type="button"
-      variant="outline"
-      className="w-full"
-      onClick={() => {
-        signInWithGoogle();
-      }}
-    >
-      Login with Google
-    </Button>
-  );
+  const [user, setUser] = useState<any>(null);
+  const supabase = createClient();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
+  if (user === null) {
+    return (
+      <Button
+        type="button"
+        variant="outline"
+        className="w-full"
+        onClick={() => {
+          signInWithGoogle();
+        }}
+      >
+        Login with Google
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        type="button"
+        variant="default"
+        className="w-full"
+        onClick={() => {
+          redirect('/calendar');
+        }}
+      >
+        View Calendar
+      </Button>
+    );
+  }
 };
 
 export default SignInWithGoogleButton;
